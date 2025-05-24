fa9ae66d02f7899a54388b6f2dda8cdefbd06516
Shulhan <18221051@std.stei.itb.ac.id>
2025-05-24 18:50:31 +0700

    new main ipynb for clearer code

--- function.py
+++ function.py
@@ -5,6 +5,7 @@ import neal
 import numpy as np
 from sklearn.metrics import mean_squared_error as mse
 from sklearn.preprocessing import PolynomialFeatures
+from collections import defaultdict
 
 import networkx as nx
 from collections import defaultdict
@@ -21,8 +22,20 @@ def multiplier(x):
     else:
         return -1
 
+# Function to convert data to polynomial form
+def polynomialForm(X, d, dim, precision, degree):
+    poly = PolynomialFeatures(degree=degree)
+    X = poly.fit_transform(X)
+    d = len(X[0])
+    dim = d * (2 * precision)
+
+    return X, d, dim
+
+
+# These function below is called if you want to solve using neal simulated annealing
 # Generate QUBO matrix    
-def generateQuboMatrix(XtX, XtY, precision, Q, d):
+def generateQuboMatrix_neal(XtX, XtY, precision, d):
+    Q = defaultdict(int)
     # First term, same weights
     for i in range(d):
         xii = XtX[i, i]
@@ -51,6 +64,8 @@ def generateQuboMatrix(XtX, XtY, precision, Q, d):
             d1 = i * 2 * precision + k
             Q[(d1, d1)] -= 2 * xyi / pow(2, k % precision) * multiplier(k)
 
+    return Q
+
 # Perform quantum annealing    
 def sampling(Q):
     sampler = neal.SimulatedAnnealingSampler()
@@ -74,14 +89,51 @@ def solve_neal(sampleset, dim, precision, d):
     
     return distributions, wts
 
-def solve_fixstar(Q, dim, precision, d):
+
+# These function below is called if you want to solve using fixstar simulated annealing
+# Generate QUBO matrix
+def generateQuboMatrix_fixstar(XtX, XtY, dim, precision, d):
     gen = VariableGenerator()
     x = gen.array("Binary", dim) 
-    
     model = Poly(0.0)
-    for (i, j), coeff in Q.items():
-        model += coeff * x[i] * x[j] if i != j else coeff * x[i]
+    
+    # First term, same weights
+    for i in range(d):
+        xii = XtX[i, i]
+        for k in range(2 * precision):
+            d1 = i * 2 * precision + k
+            # Diagonal term
+            model += xii / pow(2, 2 * (k % precision)) * x[d1]
+            
+            # Off-diagonal terms
+            for l in range(k + 1, 2 * precision):
+                d2 = i * 2 * precision + l
+                coeff = 2 * xii / pow(2, k % precision + l % precision) * multiplier(k) * multiplier(l)
+                model += coeff * x[d1] * x[d2]
 
+    # First term, different weights
+    for i in range(d):
+        for j in range(i + 1, d):
+            xij = XtX[i, j]
+            for k in range(2 * precision):
+                for l in range(2 * precision):
+                    d1 = i * 2 * precision + k
+                    d2 = j * 2 * precision + l
+                    coeff = 2 * xij / pow(2, k % precision + l % precision) * multiplier(k) * multiplier(l)
+                    model += coeff * x[d1] * x[d2]
+
+    # Second Term
+    for i in range(d):
+        xyi = XtY[i]
+        for k in range(2 * precision):
+            d1 = i * 2 * precision + k
+            coeff = -2 * xyi / pow(2, k % precision) * multiplier(k)
+            model += coeff * x[d1]
+
+    return model
+
+# Extract the best solution 
+def solve_fixstar(model, dim, precision, d):
     client = FixstarsClient()
     client.token = "AE/cM6820MJeJvPqvnxltdqlGcSDFAuP7PN"
     client.parameters.timeout = 5000
@@ -103,12 +155,4 @@ def solve_fixstar(Q, dim, precision, d):
 
     return distributions, wts
 
-# Function to convert data to polynomial form
-def polynomialForm(X, d, dim, precision, degree):
-    poly = PolynomialFeatures(degree=degree)
-    X = poly.fit_transform(X)
-    d = len(X[0])
-    dim = d * (2 * precision)
-
-    return X, d, dim
     
